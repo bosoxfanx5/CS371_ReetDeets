f92401d9fef45154bdac373a34c7ca7f38060822
bosoxfanx5 <rob15047@byui.edu>
2017-06-07 17:50:48 -0700

    Create session.php

--- web/session.php
+++ web/session.php
@@ -4,11 +4,11 @@
 //
 // session_cache_expire(15);
 // $cache_expire = session_cache_expire();
-// session_start();
-// if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
-//     // last request was more than 30 minutes ago
-//     session_unset();     // unset $_SESSION variable for the run-time
-//     session_destroy();   // destroy session data in storage
-// }
-// $_SESSION['LAST_ACTIVITY'] = time();
+session_start();
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time
+    session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time();
 ?>
