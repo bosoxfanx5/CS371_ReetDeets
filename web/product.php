436e7202c8670a9eb87ef60d0345c20b59e73a23
bosoxfanx5 <rob15047@byui.edu>
2017-06-19 23:17:31 -0700

    Add files via upload

--- web/product.php
+++ web/product.php
@@ -61,10 +61,10 @@ if (!empty($_GET["barcode"])) {
       }
    }
 
-   foreach ($visitedID as $code) {
-      echo $code;
-   }
-   echo $max;
+   // foreach ($visitedID as $code) {
+   //    echo $code;
+   // }
+   // echo $max;
 }
 
 	$sql2 = $db->prepare("SELECT id FROM s_saleable_item WHERE barcode='$barcode'");
@@ -261,7 +261,7 @@ $database = null;
       <div class="row">
          <form method="GET" action="product.php?previous=true">
             <div class="col-xs-4">
-               <?php if ($previousEnabled) : ?>
+               <?php if ($previousEnabled == false) : ?>
                   <button class="btn btn-danger btn-lg" type="submit" disabled><span>Previous</span></button>
                <?php else : ?>
                   <button class="btn btn-danger btn-lg" type="submit"><span>Previous</span></button>
@@ -270,7 +270,7 @@ $database = null;
          </form>
          <form method="GET" action="product.php?next=true">
             <div class="col-xs-4">
-               <?php if (empty($next)) : ?>
+               <?php if ($nextEnabled == false) : ?>
                   <button class="btn btn-success btn-lg" type="submit" disabled><span>Next</span></button>
                <?php else :?>
                   <button class="btn btn-success btn-lg" type="submit"><span>Next</span></button>
