e5d5e277376d4663c719217ae256a9051fa5376c
bosoxfanx5 <rob15047@byui.edu>
2017-06-19 20:18:57 -0700

    Add files via upload

--- web/authForms.php
+++ web/authForms.php
@@ -1,22 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<meta name="description" content="Front-end UI">
-	<meta name="author" content="Brooks Robison">
-	<link rel="icon" href="/favicon.ico">
-
-	<title>Front-end UI</title>
-
-	<!-- Custom styles for this template -->
-	<link href="css/sticky-footer-navbar.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/index.css">
-	<link rel="stylesheet" href="css/login.css">
-	<!-- <link rel="stylesheet" href="login.css"> -->
-</head>
 <body>
 	<div id="creation" class="overlay-creation">
 		<div class="overlay-content">
