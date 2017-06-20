593c30a5c587fd2470c9db0c62063ee7af888af7
bosoxfanx5 <rob15047@byui.edu>
2017-06-19 19:59:15 -0700

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -150,54 +150,6 @@ _/_/_/      _/_/    _/_/_/        _/
   	<br>
 	<?php endif ?>
 
-
-
-	<div id="creation" class="overlay-creation">
-		<div class="overlay-content">
-			<div class="wrapper-creation">
-				<div class="form-group">
-				<form class="form-signin" method="POST" action="">
-					<h2>You want to join? Sweet!</h2>
-					<p>Fill out the form and click submit.</p>
-					<input type="text" class="form-control" name="fname" placeholder="First Name" required>
-					<br>
-					<input type="text" class="form-control" name="lname" placeholder="Last Name" required>
-					<br>
-					<input type="text" class="form-control" name="createEmail" placeholder="Email Address" required>
-					<br>
-					<input type="password" class="form-control" name="createPassword" placeholder="Password" required>
-					<button id="submitCreate" class="btn btn-success btn-lg" type="submit">Submit</button>
-					<a href="javascript:void(0)" id="closeCreate" class="closebtn">&times;</a>
-				</form>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-	<div id="login" class="overlay">
-		<div class="overlay-content">
-			<div class="wrapper">
-				<form class="form-signin" method="POST" action="">
-					<h2 class="form-signin-heading">Please login</h2>
-					<input type="text" class="form-control" name="email" placeholder="Email Address" required>
-					<br>
-					<input type="password" class="form-control" name="password" placeholder="Password" required>
-					<br>
-					<button class="btn btn-success btn-lg" type="submit">Login</button>
-					<a href="#" id="forgot">Forgot Password</a>
-					<a href="javascript:void(0)" id="closeLogin" class="closebtn">&times;</a>
-					<?php if (!$userFound) {
-						echo "<br><br><p id='loginError'>*Email address and/or password is incorrect.</p>";
-					}
-					?>
-				</form>
-			</div>
-		</div>
-	</div>
-
-	<br><br>
-
 <!--
 _/_/_/_/    _/_/      _/_/    _/_/_/_/_/  _/_/_/_/  _/_/_/
 _/        _/    _/  _/    _/      _/      _/        _/    _/
