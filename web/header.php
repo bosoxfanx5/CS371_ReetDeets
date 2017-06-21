fe4ed4a4886d74fd82a54e884cb373bdc7d8d6f1
bosoxfanx5 <rob15047@byui.edu>
2017-06-20 17:18:40 -0700

    Add files via upload

---
+++ web/header.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="RD|ReetDeets">
+	<meta name="author" content="Brooks Robison">
+	<link rel="icon" href="/favicon.ico">
+
+	<title>Front-end UI</title>
+
+	<!-- Custom styles for this template -->
+	
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/login.css">
+</head>
