514bc6e5aa41043e55442535d1124e9569627de0
bosoxfanx5 <rob15047@byui.edu>
2017-06-21 18:14:24 -0700

    Add files via upload

--- web/authenticate.php
+++ web/authenticate.php
@@ -112,7 +112,7 @@ $database = null;
 <div id="login" class="overlay">
 	<div class="overlay-content">
 		<div class="wrapper">
-			<form class="form-signin" method="POST" action="">
+			<form class="form-signin" method="POST" action="index.php">
 				<h2 class="form-signin-heading">Please login</h2>
 				<input type="text" class="form-control" name="email" placeholder="Email Address" required>
 				<br>
