d413ea89082d04072c7f062ddf65ba347e560f77
Brooks Robison <brooksrobison@Brookss-MacBook-Pro.local>
2017-07-11 18:09:28 -0700

    insert break between create and login

--- web/index.php
+++ web/index.php
@@ -123,7 +123,7 @@ _/_/_/      _/_/    _/_/_/        _/
         		<button class="btn btn-warning btn-lg" id="loginBtn">Login</button>
       	</div>
       	<div class="col-xs-6">
-         	<button class="btn btn-primary btn-lg" id="createNew">Create Login</button>
+         	<button class="btn btn-primary btn-lg" id="createNew">Create<br>Login</button>
       	</div>
     	</div>
   	</div>
