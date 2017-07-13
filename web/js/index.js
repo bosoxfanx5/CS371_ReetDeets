bc552cbd387340087deffaf4d6e0c349eec49c93
Brooks Robison <brooksrobison@Brookss-MacBook-Pro.local>
2017-07-13 12:51:39 -0700

    removed scrollTo method. Wasn't working.

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	// $("html, body").animate({ scrollTop: 1000 }, 2000);
-	window.scrollTo(0,1);
 	$("#forgot").click(function() {
 		$("#resetPass").fadeIn("slow");
 
